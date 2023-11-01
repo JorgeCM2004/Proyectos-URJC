d3ac6144e05d159a991fa0cdbe2ff1d6a707909c
JorgeCM <harryj3699@gmail.com>
2023-11-01 14:45:57 +0100

    Problema D corregido?

--- Python/Algoritmia/Universidad/Juez/DivideYVenceras/ProblemaD.py
+++ Python/Algoritmia/Universidad/Juez/DivideYVenceras/ProblemaD.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 def matar_jugadores(array: list, i: int, f: int, buscar: int) -> int:
     k = (i + f) // 2
     if array[k] == buscar or i == f:
@@ -7,54 +6,24 @@ def matar_jugadores(array: list, i: int, f: int, buscar: int) -> int:
         return k
     else:
         if array[k] > buscar:
-            return matar_jugadores(array, i, k - 1, buscar)
+            return matar_jugadores(array, i, k, buscar)
         else:
             return matar_jugadores(array, k + 1, f, buscar)
-
-def minimo_encontradoX(array: list, index: int) -> int:
-    copy_indexsup = index
-    tamaño = len(array) - 1
-    outofrangesup = False
-    outofrangeinf = False
-    while not outofrangesup and array[copy_indexsup] == 'X':
-        copy_indexsup += 1
-        if copy_indexsup > tamaño:
-            outofrangesup = True
-    if not outofrangesup:
-        return copy_indexsup
-    copy_indexinf = index
-    while not outofrangeinf and array[copy_indexinf] == 'X':
-        copy_indexinf -= 1
-        if copy_indexinf < 0:
-            outofrangeinf = True
-    if not outofrangesup:
-        return copy_indexsup
-    else:
-        return -1
 
 num = int(input())
 aux = ""
 for i in range(num):
     aux += input() + " "
 lista = list(map(int, aux.split()))
-lista_modificable = deepcopy(lista)
 eliminar = list(map(int, input().strip().split()))
-no_llenaX = True
-recorre = 0
-while no_llenaX and recorre < len(eliminar):
-    i = eliminar[recorre]
-    index = matar_jugadores(lista, 0, num ** 2 - 1, i)
-    if lista_modificable[index] == 'X':
-        index = minimo_encontradoX(lista_modificable, index)
-        if index != -1:
-            lista_modificable[index] = 'X'
-        else:
-            no_llenaX = False
-    else:
-        lista_modificable[index] = 'X'
-    recorre += 1
+index_ant = 0
+for i in eliminar:
+    index = matar_jugadores(lista, index_ant, num ** 2 - 1, i)
+    index_ant = index + 1
+    lista[index] = 'X'
+
 for i in range(num):
     for j in range(num):
         index = num * i + j
-        print(lista_modificable[index], end= ' ')
+        print(lista[index], end= ' ')
     print()
