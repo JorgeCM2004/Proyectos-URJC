de75d87bd56907421daa8d7f4953062bc34debae
JorgeCM <harryj3699@gmail.com>
2023-11-02 16:08:17 +0100

    No se por qué me da Run error

--- Python/Algoritmia/Universidad/Juez/DivideYVenceras/ProblemaD.py
+++ Python/Algoritmia/Universidad/Juez/DivideYVenceras/ProblemaD.py
@@ -1,21 +1,21 @@
-def matar_jugadores(array: list, i: int, f: int, buscar: int) -> int:
+def matar_jugadores(array: list, array_aux: list, i: int, f: int, buscar: int) -> int:
     k = (i + f) // 2
-    if array[k] == buscar or i == f:
-        if k + 1 < len(array) - 1 and array[k] < buscar:
+    if array[k] == buscar or i >= f:
+        if array_aux[k] == 'X' or array[k] < buscar:
             return k + 1
         return k
     else:
         if array[k] > buscar:
-            return matar_jugadores(array, i, k, buscar)
+            return matar_jugadores(array, array_aux, i, k - 1, buscar)
         else:
-            return matar_jugadores(array, k + 1, f, buscar)
+            return matar_jugadores(array, array_aux, k + 1, f, buscar)
 
 num = int(input())
-aux = ""
+lista = []
 for i in range(num):
-    aux += input() + " "
-lista = list(map(int, aux.split()))
+    lista.extend(list(map(int, input().split())))
 eliminar = list(map(int, input().strip().split()))
+lista_aux = [0] * num ** 2
 index_ant = 0
 for i in eliminar:
     index = matar_jugadores(lista, index_ant, num ** 2 - 1, i)
@@ -25,5 +25,8 @@ for i in eliminar:
 for i in range(num):
     for j in range(num):
         index = num * i + j
-        print(lista[index], end= ' ')
+        if lista_aux[index] == 0:
+            print(lista[index], end= ' ')
+        else:
+            print(lista_aux[index], end= ' ')
     print()
