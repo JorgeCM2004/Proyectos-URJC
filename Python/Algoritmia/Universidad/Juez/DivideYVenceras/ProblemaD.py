732385048d2f077560bed067e95680e59665aecc
JorgeCM <harryj3699@gmail.com>
2023-11-07 15:59:46 +0100

    Problema D Solucionado

--- Python/Algoritmia/Universidad/Juez/DivideYVenceras/ProblemaD.py
+++ Python/Algoritmia/Universidad/Juez/DivideYVenceras/ProblemaD.py
@@ -1,14 +1,14 @@
-def matar_jugadores(array: list, array_aux: list, i: int, f: int, buscar: int) -> int:
+def matar_jugadores(array: list, i: int, f: int, buscar: int) -> int:
     k = (i + f) // 2
     if array[k] == buscar or i >= f:
-        if array_aux[k] == 'X' or array[k] < buscar:
+        if array[k] < buscar and k + 1 < len(array) - 1:
             return k + 1
         return k
     else:
         if array[k] > buscar:
-            return matar_jugadores(array, array_aux, i, k - 1, buscar)
+            return matar_jugadores(array, i, k - 1, buscar)
         else:
-            return matar_jugadores(array, array_aux, k + 1, f, buscar)
+            return matar_jugadores(array, k + 1, f, buscar)
 
 num = int(input())
 lista = []
@@ -16,11 +16,18 @@ for i in range(num):
     lista.extend(list(map(int, input().split())))
 eliminar = list(map(int, input().strip().split()))
 lista_aux = [0] * num ** 2
-index_ant = 0
-for i in eliminar:
-    index = matar_jugadores(lista, index_ant, num ** 2 - 1, i)
-    index_ant = index + 1
-    lista[index] = 'X'
+recorrer = 0
+noacabado = True
+while recorrer < len(eliminar) and noacabado:
+    i = eliminar[recorrer]
+    index = matar_jugadores(lista, 0, num ** 2 - 1, i)
+    while index < len(lista_aux) and lista_aux[index] == 'X':
+        index += 1
+    if index > len(lista_aux) - 1:
+        noacabado = False
+    else:
+        lista_aux[index] = 'X'
+    recorrer += 1
 
 for i in range(num):
     for j in range(num):
