1a9e1e4e58cbcd4d8fac50d1fd05a511fe2fb41e
JorgeCM <harryj3699@gmail.com>
2023-11-11 22:40:46 +0100

    Retoques menores n-damas
    Correccion cuando no hay solucion posible, y interfaz mas entendible para el usuario

--- Python/Algoritmia/Universidad/Asignatura/Backtraking/N_Damas.py
+++ Python/Algoritmia/Universidad/Asignatura/Backtraking/N_Damas.py
@@ -30,20 +30,23 @@ def factible(l: list) -> bool:
 def conflicto(xdama1: int, ydama1: int, xdama2: int, ydama2: int):
     return (xdama1 + ydama1) == (xdama2 + ydama2) or (xdama1 - ydama1) == (xdama2 - ydama2) or xdama1 == xdama2 or ydama1 == ydama2
 
-num = int(input())
+num = int(input("Introduzca cuantas damas (n) se deberian colocar en el tablero (n * n) sin que se amenacen entre ellas: "))
 lista = [None] * num
-repuesta = backtracking(lista, num, 0)
+respuesta = backtracking(lista, num, 0)
 blanco = True
-for x in range(num):
-    for y in range(num):
-        if repuesta[x] == y:
-            print('👑', end='')
-        else:
-            if blanco:
-                print('⬜', end='')
+if respuesta:
+    for x in range(num):
+        for y in range(num):
+            if respuesta[x] == y:
+                print('👑', end='')
             else:
-                print('⬛', end='')
-        blanco = not blanco
-    if num % 2 == 0:
-        blanco = not blanco
-    print()
+                if blanco:
+                    print('⬜', end='')
+                else:
+                    print('⬛', end='')
+            blanco = not blanco
+        if num % 2 == 0:
+            blanco = not blanco
+        print()
+else:
+    print(f"No existe solucion posible para el problema de las {num} damas.")
